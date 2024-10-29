35b61b3bbec88f1d417d4a58dd76046e0277ab9a
Josué de Oliveira Mendonça <kanji.flimorago@gmail.com>
2024-10-29 15:48:25 -0300

    Update startserver.bat

--- startserver.bat
+++ startserver.bat
@@ -2,6 +2,7 @@ cls
 @echo off 
 cd "C:\Users\josue_o_mendonca\Documents\JoJoRPG"
 cls
+start chrome.exe "http://localhost:3001/cadastro-login"
+cls
 echo CTRL + C para fechar o programa
-start JoJoRPG.py
 npm start
